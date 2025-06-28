bdde5332bdb53f6c50b173126660650d621bfbd4
mevanl <mevanlauderdaleyt@gmail.com>
2025-06-28 12:56:21 -0500

    Added button toggle for country borders

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
 import { initMap } from './map/mapManager.js'
 import { loadPoliticalMapLayer } from './map/layers/political.js'
 
+// default state
+let bordersVisible = true
+
+
 const map = initMap()
 
 // wait for map to fully load before adding layers 
 map.on('load', () => {
   loadPoliticalMapLayer(map)
+})
+
+document.getElementById('button_toggle_country_borders').addEventListener('click', () => {
+  const visibility = bordersVisible ? 'none' : 'visible'
+  map.setLayoutProperty('political-border-line', 'visibility', visibility)
+  bordersVisible = !bordersVisible
+
+  document.getElementById('button_toggle_country_borders').innerText =
+    bordersVisible ? 'Hide Borders' : 'Show Borders'
 })
